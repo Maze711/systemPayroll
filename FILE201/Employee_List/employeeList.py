96f5c5f6d90940a40893007b3df60222e50233c8
Maze711 <badlonmazeclarion@gmail.com>
2024-06-19 17:40:34 +0800

    Added a Pie Graph using matplotlib library

--- FILE201/Employee_List/employeeList.py
+++ FILE201/Employee_List/employeeList.py
@@ -1,10 +1,13 @@
 import sys
-import os
 import mysql.connector
+import os
+
+from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout
 from PyQt5.uic import loadUi
-from FILE201.file201_Function.pieGraph import MplCanvas, graphLoader
 
+from FILE201.file201_Function.pieGraph import MplCanvas, graphLoader
+from FILE201.file201_Function.listFunction import ListFunction
 
 class EmployeeList(QMainWindow):
     def __init__(self):
@@ -20,4 +23,9 @@ class EmployeeList(QMainWindow):
         self.graph_loader = graphLoader(self.canvas)
         self.graph_loader.plot_pie_chart()
 
+        self.functions = ListFunction(self)
 
+        # Calls the timeClock recursively every 1 second
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.functions.timeClock)
+        self.timer.start(1)
