956ede9699a7ff817afd8f4d96d5751a4a24b1da
Maze711 <badlonmazeclarion@gmail.com>
2024-06-24 17:32:30 +0800

    Modified View Personal Information fetch

--- FILE201/file201_Function/listFunction.py
+++ FILE201/file201_Function/listFunction.py
@@ -1,7 +1,9 @@
-from PyQt5.QtCore import QTimer
+from PyQt5.QtCore import QTimer, QDate
 from PyQt5.QtWidgets import QTableWidgetItem
 from time import *
 import logging
+
+from PyQt5.uic.properties import QtCore
 from mysql.connector import Error
 
 # Configure logging
@@ -82,10 +84,24 @@ class ListFunction:
 
     def fetch_employee_data(self, empID):
         query = """
-           SELECT lastName, firstName, middleName
-           FROM personal_information
-           WHERE empID = %s
-           """
+            SELECT p.lastName, p.firstName, p.middleName, p.street, p.barangay, p.city, p.province, p.zip, 
+                   p.phoneNum, p.height, p.weight, p.civilStatus, p.dateOfBirth, p.placeOfBirth, p.gender,
+                   f.fathersLastName, f.fathersFirstName, f.fathersMiddleName, f.mothersLastName, 
+                   f.mothersFirstName, f.mothersMiddleName, f.spouseLastName, f.spouseFirstName, 
+                   f.spouseMiddleName, f.beneficiaryLastName, f.beneficiaryFirstName, f.beneficiaryMiddleName, 
+                   f.dependentsName,
+                   i.sssNum, i.pagibigNum, i.philhealthNum, i.tinNum,
+                   w.fromDate, w.toDate, w.companyName, w.companyAdd, w.empPosition,
+                   e.techSkill, e.certificateSkill, e.validationDate, e.college, e.highSchool, e.elemSchool,
+                   e.collegeAdd, e.highschoolAdd, e.elemAdd, e.collegeCourse, e.highschoolStrand, e.collegeYear,
+                   e.highschoolYear, e.elemYear
+                   FROM personal_information p
+                   LEFT JOIN family_background f ON p.empID = f.empID
+                   LEFT JOIN list_of_id i ON p.empID = i.empID
+                   LEFT JOIN work_exp w ON p.empID = w.empID
+                   LEFT JOIN educ_information e ON p.empID = e.empID
+                   WHERE p.empID = %s
+            """
         try:
             connection = create_connection()
             if connection is None:
@@ -96,7 +112,7 @@ class ListFunction:
             cursor.execute(query, (empID,))
             result = cursor.fetchone()
             if result:
-                return result
+                return result  # Returns a tuple with all fetched columns
             return None
 
         except Error as e:
@@ -110,11 +126,82 @@ class ListFunction:
                 logger.info("Database connection closed")
 
     def populate_modal_with_employee_data(self, modal, data):
-        lastName, firstName, middleName = data
+        try:
+            (lastName, firstName, middleName, street, barangay, city, province, zip,
+             phoneNum, height, weight, civilStatus, dateOfBirth, placeOfBirth, gender,
+             fathersLastName, fathersFirstName, fathersMiddleName, mothersLastName, mothersFirstName, mothersMiddleName,
+             spouseLastName, spouseFirstName, spouseMiddleName, beneficiaryLastName, beneficiaryFirstName,
+             beneficiaryMiddleName, dependentsName,
+             sss_num, pagibig_num, philhealth_num, tin_num,
+             from_date, to_date, company_name, company_add, position,
+             tech_skill, certificate_skill, validation_date, college, high_school, elem_school,
+             college_add, highschool_add, elem_add, college_course, highschool_strand, college_year,
+             highschool_year, elem_year) = data
 
-        modal.txtLastName.setText(lastName)
-        modal.txtFirstName.setText(firstName)
-        modal.txtMiddleName.setText(middleName)
+            modal.txtLastName.setText(lastName)
+            modal.txtFirstName.setText(firstName)
+            modal.txtMiddleName.setText(middleName)
+            modal.txtStreet.setText(street)
+            modal.txtBarangay.setText(barangay)
+            modal.txtCity.setText(city)
+            modal.txtProvince.setText(province)
+            modal.txtZip.setText(zip)
+            modal.txtPhone.setText(phoneNum)
+            modal.txtHeight.setText(str(height))
+            modal.txtWeight.setText(str(weight))
+            modal.cmbCivil.setCurrentText(civilStatus)
+            modal.dtDateOfBirth.setDate(QDate.fromString(dateOfBirth, "MM-dd-yyyy"))
+            modal.txtPlace.setText(placeOfBirth)
+            modal.cmbGender.setCurrentText(gender)
+
+            modal.txtFatherLast.setText(fathersLastName)
+            modal.txtFatherFirst.setText(fathersFirstName)
+            modal.txtFatherMiddle.setText(fathersMiddleName)
+            modal.txtMotherLast.setText(mothersLastName)
+            modal.txtMotherFirst.setText(mothersFirstName)
+            modal.txtMotherMiddle.setText(mothersMiddleName)
+            modal.txtSpouseLast.setText(spouseLastName)
+            modal.txtSpouseFirst.setText(spouseFirstName)
+            modal.txtSpouseMiddle.setText(spouseMiddleName)
+            modal.txtBeneLast.setText(beneficiaryLastName)
+            modal.txtBeneFirst.setText(beneficiaryFirstName)
+            modal.txtBeneMiddle.setText(beneficiaryMiddleName)
+            modal.txtDependent.setText(dependentsName)
+
+            modal.sssTextEdit.setPlainText(sss_num)
+            modal.pagibigTextEdit.setPlainText(pagibig_num)
+            modal.philHealthTextEdit.setPlainText(philhealth_num)
+            modal.tinTextEdit.setPlainText(tin_num)
+
+            modal.dateStart_4.setDate(QDate.fromString(from_date, "MM-dd-yyyy"))
+            modal.dateEnd_4.setDate(QDate.fromString(to_date, "MM-dd-yyyy"))
+            modal.companyTextEdit_4.setPlainText(company_name)
+            modal.addressTextEdit_4.setPlainText(company_add)
+            modal.positionTextEdit_4.setPlainText(position)
+
+            modal.techSkillTextEdit.setPlainText(tech_skill)
+            modal.certiTextEdit1.setPlainText(certificate_skill)
+            modal.validationDate1.setDate(QDate.fromString(validation_date, "MM-dd-yyyy"))
+
+            modal.collegeTextEdit.setPlainText(college)
+            modal.highTextEdit.setPlainText(high_school)
+            modal.elemTextEdit.setPlainText(elem_school)
+
+            modal.addressTextEdit.setPlainText(college_add)
+            modal.addressTextEdit2.setPlainText(highschool_add)
+            modal.addressTextEdit3.setPlainText(elem_add)
+
+            modal.courseTextEdit.setPlainText(college_course)
+            modal.courseTextEdit2.setPlainText(highschool_strand)
+
+            modal.schoolYear.setDate(QDate.fromString(college_year, "MM-dd-yyyy"))
+            modal.schoolYear2.setDate(QDate.fromString(highschool_year, "MM-dd-yyyy"))
+            modal.schoolYear3.setDate(QDate.fromString(elem_year, "MM-dd-yyyy"))
+
+
+        except Exception as e:
+            logger.error(f"Error populating modal with employee data: {e}")
+            print(f"Error: {e}")
 
     def open_otherInformationMODAL_add(self):
         modal = personalModal()
