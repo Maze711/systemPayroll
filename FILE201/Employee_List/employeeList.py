b12f48c72f1fd8fb8e80142c8ca573d945b6f363
s3nkuuu <jhayemcalleja011@gmail.com>
2024-06-25 02:42:49 +0800

    ExcelExporter

--- FILE201/Employee_List/employeeList.py
+++ FILE201/Employee_List/employeeList.py
@@ -2,13 +2,14 @@ import sys
 import os
 
 from PyQt5.QtCore import QTimer
-from PyQt5.QtWidgets import QMainWindow, QApplication, QVBoxLayout, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QApplication, QVBoxLayout, QPushButton, QFileDialog
 from PyQt5.uic import loadUi
 
 from FILE201.file201_Function.pieGraph import MplCanvas, graphLoader
 from FILE201.file201_Function.listFunction import ListFunction
 from FILE201.Other_Information.otherInformationModal import personalModal
 from FILE201.file201_Function.modalFunction import modalFunction
+from FILE201.file201_Function.excelExport import fetch_personal_information, export_to_excel
 
 class EmployeeList(QMainWindow):
     def __init__(self):
@@ -47,3 +48,16 @@ class EmployeeList(QMainWindow):
 
         # Connect search function to QLineEdit
         self.txtSearch.textChanged.connect(self.functions.searchEmployees)
+
+        # Connect export button to the export function
+        self.btnExport.clicked.connect(self.export_to_excel)
+
+    def export_to_excel(self):
+        data, columns = fetch_personal_information()
+        if data:
+            # Open a file dialog to select where to save the Excel file
+            options = QFileDialog.Options()
+            file_name, _ = QFileDialog.getSaveFileName(self, "Save As", "", "Excel Files (*.xlsx);;All Files (*)",
+                                                       options=options)
+            if file_name:
+                export_to_excel(data, columns, file_name)
