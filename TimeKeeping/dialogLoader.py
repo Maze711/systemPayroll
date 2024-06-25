ecdc1b73359b65a128d6e81fb2bb8f48a971f571
Maze711 <badlonmazeclarion@gmail.com>
2024-06-25 17:05:51 +0800

    Created a table to show inside of import

--- TimeKeeping/dialogLoader.py
+++ TimeKeeping/dialogLoader.py
@@ -1,7 +1,8 @@
 import sys
 import os
 import logging
-from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog
+from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog, QMainWindow, QVBoxLayout, QTableWidget, \
+    QTableWidgetItem, QWidget
 from PyQt5.uic import loadUi
 
 # Configure the logger
@@ -17,15 +18,50 @@ class dialogModal(QDialog):
         self.importBTN.clicked.connect(self.importTxt)
 
     def importTxt(self):
-        fileName, _ = QFileDialog.getOpenFileName(self, "Open Text File", "",
-                                                  "DAT Files (*.DAT);;Text Files (*.txt);;All Files (*)")
+        fileName, _ = QFileDialog.getOpenFileName(self, "Open Text or DAT File", "",
+                                                  "Text Files (*.txt);;DAT Files (*.DAT);;All Files (*)")
         if fileName:
             try:
                 with open(fileName, 'r') as file:
                     content = file.read()
                     logging.info(f"File {fileName} content:\n{content}")
+                    self.showData(content)
             except Exception as e:
                 logging.error(f"Failed to read file {fileName}: {e}")
+
+    def showData(self, content):
+        frame = QMainWindow()
+        frame.setWindowTitle("Imported Data")
+        frame.setGeometry(100, 100, 600, 400)
+        central_widget = QWidget(frame)
+        layout = QVBoxLayout(central_widget)
+
+        table = QTableWidget()
+        table.setRowCount(0)
+        table.setColumnCount(4)
+
+        # Set table headers
+        headers = ["Bio No.", "TransDate", "Time", "MachCode"]
+        table.setHorizontalHeaderLabels(headers)
+
+        # Populate the table with data
+        rows = content.strip().split('\n')
+        for row in rows:
+            columns = row.split('\t')
+            bio_no = columns[0].strip()
+            trans_date, time = columns[1].strip().split(' ')
+            mach_code = columns[2].strip()
+            row_position = table.rowCount()
+            table.insertRow(row_position)
+            table.setItem(row_position, 0, QTableWidgetItem(bio_no))
+            table.setItem(row_position, 1, QTableWidgetItem(trans_date))
+            table.setItem(row_position, 2, QTableWidgetItem(time))
+            table.setItem(row_position, 3, QTableWidgetItem(mach_code))
+
+        layout.addWidget(table)
+        frame.setCentralWidget(central_widget)
+        frame.show()
+        self.frame = frame
 
 
 if __name__ == '__main__':
