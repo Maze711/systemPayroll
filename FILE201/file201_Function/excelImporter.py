9cd1257c442c9cb50128c188011d562dea8ed058
Maze711 <badlonmazeclarion@gmail.com>
2024-07-23 01:08:23 +0800

    Updated excel importer

--- FILE201/file201_Function/excelImporter.py
+++ FILE201/file201_Function/excelImporter.py
@@ -7,24 +7,18 @@ import logging
 from PyQt5.QtWidgets import QFileDialog, QMessageBox
 from MainFrame.Database_Connection.DBConnection import create_connection
 
-def format_time(time_str):
+def format_birthday(birthday_str):
     try:
-        time_obj = datetime.strptime(time_str, '%I%p')
-        formatted_time = time_obj.strftime('%I:%M %p')
-        return formatted_time
+        date_obj = datetime.strptime(birthday_str, '%d-%b-%y')
+        formatted_date = date_obj.strftime('%Y-%m-%d')
+        return formatted_date
     except ValueError:
-        try:
-            time_obj = datetime.strptime(time_str, '%I:%M %p')
-            formatted_time = time_obj.strftime('%I:%M %p')
-            return formatted_time
-        except ValueError:
-            return time_str
+        return '0000-00-00'
 
 def importIntoDB(parent, display_employees_callback):
     try:
         options = QFileDialog.Options()
         file_name, _ = QFileDialog.getOpenFileName(parent, "Select Excel File", "", "Excel Files (*.xls *.xlsx)")
-
         if not file_name:
             return
 
@@ -32,25 +26,26 @@ def importIntoDB(parent, display_employees_callback):
         sheet = workbook.sheet_by_index(0)
 
         required_personal_columns = [
-            'empno', 'surname', 'firstname', 'mi', 'emp_id', 'addr1', 'mobile', 'birthday', 'civil_stat', 'gender',
-            'email', 'emer_name', 'height', 'weight', 'birthplace', 'religion', 'citizenship', 'blood_type'
+            'empl_no', 'empl_id', 'idnum', 'empl_name', 'street', 'city', 'zipcode',
+            'birthday', 'status', 'sex', 'telephone'
+        ]
+        required_emergency_columns = [
+            'empl_no', 'empl_id'
         ]
         required_list_columns = [
-            'emp_id', 'sssno', 'tin', 'pagibig', 'philhealth', 'txcode'
-        ]
-        required_posnsched_columns = [
-            'emp_id', 'pos_descr', 'sche_name', 'dept_name'
+            'empl_no', 'empl_id', 'taxstat', 'sss', 'tin', 'pagibig', 'philhealth', 'account_no'
         ]
         required_empstatus_columns = [
-            'emp_id', 'status'
+            'empl_no', 'empl_id', 'compcode', 'dept_code', 'position', 'emp_stat', 'date_hired', 'resigned', 'dtresign'
         ]
-        required_vacnsic_columns = [
-            'emp_id', 'max_vacn', 'max_sick'
+        required_rate_columns = [
+            'empl_no', 'empl_id', 'rph', 'rate', 'mth_salary', 'dailyallow', 'mntlyallow', 'ssloanded',
+            'ssloanamt', 'lovelnded', 'lovelnamt'
         ]
 
         headers = [sheet.cell_value(0, col) for col in range(sheet.ncols)]
-        missing_columns = [col for col in required_personal_columns + required_list_columns
-                           + required_posnsched_columns + required_empstatus_columns + required_vacnsic_columns
+        missing_columns = [col for col in required_personal_columns + required_emergency_columns + required_list_columns
+                           + required_empstatus_columns + required_rate_columns
                            if col not in headers]
 
         if missing_columns:
@@ -64,133 +59,123 @@ def importIntoDB(parent, display_employees_callback):
 
         cursor = connection.cursor()
 
-        acct_no_mapping = {
-            'emp_id': 'emp_id',
-            'paycode': 'paycode',
-            'acct_no': 'acct_no',
-            'bank_code': 'bank_code',
-            'cola': 'cola'
+        emergency_list_mapping = {
+            'empl_no': 'empl_no',
+            'empl_id': 'empl_id',
         }
-        personal_column_mapping = {
-            'empno': 'empno',
-            'surname': 'surname',
-            'firstname': 'firstname',
-            'mi': 'mi',
-            'emp_id': 'emp_id',
-            'addr1': 'addr1',
-            'mobile': 'mobile',
+        emp_info_column_mapping = {
+            'empl_no': 'empl_no',
+            'empl_id': 'empl_id',
+            'idnum': 'idnum',
+            'empl_name': 'empl_name',
+            'street': 'street',
+            'city': 'city',
+            'zipcode': 'zipcode',
             'birthday': 'birthday',
-            'civil_stat': 'civil_stat',
-            'gender': 'gender',
-            'email': 'email',
-            'emer_name': 'emer_name',
-            'height': 'height',
-            'weight': 'weight',
-            'birthplace': 'birthplace',
-            'religion': 'religion',
-            'citizenship': 'citizenship',
-            'blood_type': 'blood_type'
+            'status': 'status',
+            'sex': 'sex',
+            'telephone': 'telephone',
         }
         list_column_mapping = {
-            'emp_id': 'emp_id',
-            'sssno': 'sssno',
+            'empl_no': 'empl_no',
+            'empl_id': 'empl_id',
+            'taxstat': 'taxstat',
+            'sss': 'sss',
             'tin': 'tin',
             'pagibig': 'pagibig',
             'philhealth': 'philhealth',
-            'txcode': 'txcode'
-        }
-        posnsched_column_mapping = {
-            'emp_id': 'emp_id',
-            'pos_descr': 'pos_descr',
-            'sche_name': 'sche_name',
-            'dept_name': 'dept_name'
-        }
-        empstatus_column_mapping = {
-            'emp_id': 'emp_id',
-            'status': 'status'
-        }
-        vacnsic_column_mapping = {
-            'emp_id': 'emp_id',
-            'max_vacn': 'max_vacn',
-            'max_sick': 'max_sick'
+            'account_no': 'account_no',
         }
 
-        insert_acc_no_query = """
-           INSERT INTO acct_no (emp_id, paycode, acct_no, bank_code, cola)
-           VALUES (%s, %s, %s, %s, %s)
-           """
+        empstatus_column_mapping = {
+            'empl_no': 'empl_no',
+            'empl_id': 'empl_id',
+            'compcode': 'compcode',
+            'dept_code': 'dept_code',
+            'position': 'position',
+            'emp_stat': 'emp_stat',
+            'date_hired': 'date_hired',
+            'resigned': 'resigned',
+            'dtresign': 'dtresign',
+        }
+
+        insert_emergency_query = """
+            INSERT INTO emergency_list (empl_no, empl_id)
+            VALUES (%s, %s)
+        """
         insert_personal_query = """
-           INSERT INTO personal_information (empno, surname, firstname, mi, emp_id, addr1, emer_name, mobile, height,
-                                             weight, civil_stat, birthday, birthplace, gender, email, religion, 
-                                             citizenship, blood_type)
-           VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-           """
+            INSERT INTO emp_info (empl_no, empl_id, idnum, surname, firstname, street, city, zipcode,
+                                  birthday, status, sex, telephone)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
         insert_list_query = """
-           INSERT INTO list_of_id (emp_id, sssno, tin, pagibig, philhealth, txcode)
-           VALUES (%s, %s, %s, %s, %s, %s)
-           """
-        insert_posnsched_query = """
-           INSERT INTO emp_posnsched (emp_id, pos_descr, sched_in, sched_out, dept_name)
-           VALUES (%s, %s, %s, %s, %s)
-           """
-        insert_emp_stauts_query = """
-           INSERT INTO emp_status (emp_id, status)
-           VALUES (%s, %s)
-           """
-        insert_vacnsic_query = """
-           INSERT INTO vacn_sick_count (emp_id, max_vacn, max_sick)
-           VALUES (%s, %s, %s)
-           """
+            INSERT INTO emp_list_id (empl_no, empl_id, taxstat, sss, tin, pagibig, philhealth, account_no)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        insert_emp_status_query = """
+            INSERT INTO emp_status (empl_no, empl_id, compcode, dept_code, position, emp_stat, date_hired, resigned, dtresign)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
 
         for row_idx in range(1, sheet.nrows):
             row = sheet.row_values(row_idx)
 
-            empno = row[headers.index('empno')] if 'empno' in headers else ''
+            empno = row[headers.index('empl_no')] if 'empl_no' in headers else ''
             if not empno:
-                logging.warning(f"Skipping row {row_idx + 1} due to missing empno.")
+                logging.warning(f"Skipping row {row_idx + 1} due to missing empl_no.")
                 continue
 
-            acc_no = {key: row[headers.index(acct_no_mapping[key])] if key in acct_no_mapping else '' for
-                      key in acct_no_mapping.keys()}
+            # Process empl_name into surname and firstname
+            empl_name = row[headers.index('empl_name')] if 'empl_name' in headers else ''
+            surname, firstname = '', ''
+            if ',' in empl_name:
+                surname, firstname = [name.strip() for name in empl_name.split(',', 1)]
+            else:
+                surname = empl_name.strip()
+
+            # Process birthday into YYYY-MM-DD format
+            birthday = row[headers.index('birthday')] if 'birthday' in headers else ''
+            formatted_birthday = format_birthday(birthday)
+
+            emer_data = {
+                key: row[headers.index(emergency_list_mapping[key])] if key in emergency_list_mapping else ''
+                for key in emergency_list_mapping.keys()
+            }
             personal_data = {
-                key: row[headers.index(personal_column_mapping[key])] if key in personal_column_mapping else '' for
-                key in personal_column_mapping.keys()}
-            list_data = {key: row[headers.index(list_column_mapping[key])] if key in list_column_mapping else '' for
-                         key in list_column_mapping.keys()}
-            posnsched_data = {
-                key: row[headers.index(posnsched_column_mapping[key])] if key in posnsched_column_mapping else ''
-                for key in posnsched_column_mapping.keys()
+                key: row[headers.index(emp_info_column_mapping[key])] if key in emp_info_column_mapping else ''
+                for key in emp_info_column_mapping.keys()
+            }
+            personal_data['surname'] = surname
+            personal_data['firstname'] = firstname
+            personal_data['birthday'] = formatted_birthday
+
+            list_data = {
+                key: row[headers.index(list_column_mapping[key])] if key in list_column_mapping else ''
+                for key in list_column_mapping.keys()
             }
             empstatus_data = {
                 key: row[headers.index(empstatus_column_mapping[key])] if key in empstatus_column_mapping else ''
                 for key in empstatus_column_mapping.keys()
             }
-            vacnsic_data = {
-                key: row[headers.index(vacnsic_column_mapping[key])] if key in vacnsic_column_mapping else ''
-                for key in vacnsic_column_mapping.keys()
-            }
 
             try:
-                cursor.execute(insert_acc_no_query, tuple(acc_no.values()))
-                cursor.execute(insert_personal_query, tuple(personal_data.values()))
-                cursor.execute(insert_list_query, tuple(list_data.values()))
-
-                if 'sche_name' in posnsched_data:
-                    sche_name = posnsched_data['sche_name'].strip().lower()
-                    if sche_name.endswith(' sched'):
-                        sche_name = sche_name[:-6].strip()
-
-                    sche_name_parts = sche_name.split('-')
-                    sched_in = format_time(sche_name_parts[0].strip())
-                    sched_out = format_time(sche_name_parts[1].strip()) if len(sche_name_parts) > 1 else ''
-
-                    cursor.execute(insert_posnsched_query, (
-                        posnsched_data['emp_id'], posnsched_data['pos_descr'], sched_in, sched_out,
-                        posnsched_data['dept_name']
-                    ))
-
-                cursor.execute(insert_emp_stauts_query, tuple(empstatus_data.values()))
-                cursor.execute(insert_vacnsic_query, tuple(vacnsic_data.values()))
+                cursor.execute(insert_emergency_query, (emer_data['empl_no'], emer_data['empl_id']))
+                cursor.execute(insert_personal_query, (
+                    personal_data['empl_no'], personal_data['empl_id'], personal_data['idnum'],
+                    personal_data['surname'], personal_data['firstname'], personal_data['street'],
+                    personal_data['city'], personal_data['zipcode'], personal_data['birthday'],
+                    personal_data['status'], personal_data['sex'], personal_data['telephone']
+                ))
+                cursor.execute(insert_list_query, (
+                    list_data['empl_no'], list_data['empl_id'], list_data['taxstat'],
+                    list_data['sss'], list_data['tin'], list_data['pagibig'],
+                    list_data['philhealth'], list_data['account_no']
+                ))
+                cursor.execute(insert_emp_status_query, (
+                    empstatus_data['empl_no'], empstatus_data['empl_id'], empstatus_data['compcode'],
+                    empstatus_data['dept_code'], empstatus_data['position'], empstatus_data['emp_stat'],
+                    empstatus_data['date_hired'], empstatus_data['resigned'], empstatus_data['dtresign']
+                ))
 
                 connection.commit()
 
