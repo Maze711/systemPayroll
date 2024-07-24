c411fcc7c7ced63ec8210bda38d05f09797fb9a7
Maze711 <badlonmazeclarion@gmail.com>
2024-07-24 18:18:42 +0800

    Update importer and paytimesheet

--- FILE201/file201_Function/excelImporter.py
+++ FILE201/file201_Function/excelImporter.py
@@ -7,18 +7,33 @@ import logging
 from PyQt5.QtWidgets import QFileDialog, QMessageBox
 from MainFrame.Database_Connection.DBConnection import create_connection
 
-def format_birthday(birthday_str):
+def convert_to_24hour(time_str):
     try:
-        date_obj = datetime.strptime(birthday_str, '%d-%b-%y')
-        formatted_date = date_obj.strftime('%Y-%m-%d')
-        return formatted_date
+        if isinstance(time_str, str):
+            if ':' in time_str:
+                in_time = datetime.strptime(time_str, "%I:%M %p")
+            else:
+                in_time = datetime.strptime(time_str, "%I%p")
+            return in_time.strftime("%H:%M")
+        return None
     except ValueError:
-        return '0000-00-00'
+        logging.warning(f"Unable to convert time: {time_str}")
+        return None
+
+def split_schedule(sche_name):
+    if isinstance(sche_name, str) and '-' in sche_name:
+        parts = sche_name.split('-')
+        if len(parts) == 2:
+            sched_in = convert_to_24hour(parts[0].strip())
+            sched_out = convert_to_24hour(parts[1].strip().replace("sched", "").strip())
+            return sched_in, sched_out
+    return None, None
 
 def importIntoDB(parent, display_employees_callback):
     try:
         options = QFileDialog.Options()
-        file_name, _ = QFileDialog.getOpenFileName(parent, "Select Excel File", "", "Excel Files (*.xls *.xlsx)")
+        file_name, _ = QFileDialog.getOpenFileName(
+            parent, "Select Excel File", "", "Excel Files (*.xls *.xlsx)")
         if not file_name:
             return
 
@@ -26,27 +41,23 @@ def importIntoDB(parent, display_employees_callback):
         sheet = workbook.sheet_by_index(0)
 
         required_personal_columns = [
-            'empl_no', 'empl_id', 'idnum', 'empl_name', 'street', 'city', 'zipcode',
-            'birthday', 'status', 'sex', 'telephone'
+            'empno', 'emp_id', 'surname', 'firstname', 'mi', 'addr1', 'birthday', 'birthplace', 'religion',
+            'civil_stat', 'gender', 'height', 'weight', 'mobile', 'blood_type', 'email'
         ]
         required_emergency_columns = [
-            'empl_no', 'empl_id'
+            'empno', 'emp_id', 'emer_name'
         ]
         required_list_columns = [
-            'empl_no', 'empl_id', 'taxstat', 'sss', 'tin', 'pagibig', 'philhealth', 'account_no'
+            'empno', 'emp_id', 'txcode', 'sssno', 'tin', 'pagibig', 'philhealth', 'bank_code', 'cola'
         ]
-        required_empstatus_columns = [
-            'empl_no', 'empl_id', 'compcode', 'dept_code', 'position', 'emp_stat', 'date_hired', 'resigned', 'dtresign'
-        ]
-        required_rate_columns = [
-            'empl_no', 'empl_id', 'rph', 'rate', 'mth_salary', 'dailyallow', 'mntlyallow', 'ssloanded',
-            'ssloanamt', 'lovelnded', 'lovelnamt'
+        required_posnsched_columns = [
+            'empno', 'emp_id', 'pos_descr', 'sche_name', 'dept_name'
         ]
 
         headers = [sheet.cell_value(0, col) for col in range(sheet.ncols)]
-        missing_columns = [col for col in required_personal_columns + required_emergency_columns + required_list_columns
-                           + required_empstatus_columns + required_rate_columns
-                           if col not in headers]
+        missing_columns = [col for col in required_personal_columns +
+                           required_emergency_columns + required_list_columns +
+                           required_posnsched_columns if col not in headers]
 
         if missing_columns:
             error_message = f"Missing required columns: {', '.join(missing_columns)}"
@@ -60,121 +71,127 @@ def importIntoDB(parent, display_employees_callback):
         cursor = connection.cursor()
 
         emergency_list_mapping = {
-            'empl_no': 'empl_no',
-            'empl_id': 'empl_id',
+            'empl_no': 'empno',
+            'empl_id': 'emp_id',
+            'emer_name': 'emer_name'
         }
         emp_info_column_mapping = {
-            'empl_no': 'empl_no',
-            'empl_id': 'empl_id',
-            'idnum': 'idnum',
-            'empl_name': 'empl_name',
-            'street': 'street',
-            'city': 'city',
-            'zipcode': 'zipcode',
+            'empl_no': 'empno',
+            'empl_id': 'emp_id',
+            'surname': 'surname',
+            'firstname': 'firstname',
+            'mi': 'mi',
+            'addr1': 'addr1',
             'birthday': 'birthday',
-            'status': 'status',
-            'sex': 'sex',
-            'telephone': 'telephone',
+            'birthplace': 'birthplace',
+            'religion': 'religion',
+            'status': 'civil_stat',
+            'sex': 'gender',
+            'height': 'height',
+            'weight': 'weight',
+            'mobile': 'mobile',
+            'blood_type': 'blood_type',
+            'email': 'email'
         }
         list_column_mapping = {
-            'empl_no': 'empl_no',
-            'empl_id': 'empl_id',
-            'taxstat': 'taxstat',
-            'sss': 'sss',
+            'empl_no': 'empno',
+            'empl_id': 'emp_id',
+            'taxstat': 'txcode',
+            'sss': 'sssno',
             'tin': 'tin',
             'pagibig': 'pagibig',
             'philhealth': 'philhealth',
-            'account_no': 'account_no',
+            'bank_code': 'bank_code',
+            'cola': 'cola'
         }
-
-        empstatus_column_mapping = {
-            'empl_no': 'empl_no',
-            'empl_id': 'empl_id',
-            'compcode': 'compcode',
-            'dept_code': 'dept_code',
-            'position': 'position',
-            'emp_stat': 'emp_stat',
-            'date_hired': 'date_hired',
-            'resigned': 'resigned',
-            'dtresign': 'dtresign',
+        posnsched_column_mapping = {
+            'empl_no': 'empno',
+            'empl_id': 'emp_id',
+            'pos_descr': 'pos_descr',
+            'sche_name': 'sche_name',
+            'dept_name': 'dept_name'
         }
 
         insert_emergency_query = """
-            INSERT INTO emergency_list (empl_no, empl_id)
-            VALUES (%s, %s)
+            INSERT INTO emergency_list (empl_no, empl_id, emer_name)
+            VALUES (%s, %s, %s)
         """
         insert_personal_query = """
-            INSERT INTO emp_info (empl_no, empl_id, idnum, surname, firstname, street, city, zipcode,
-                                  birthday, status, sex, telephone)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            INSERT INTO emp_info (empl_no, empl_id, surname, firstname, mi, addr1, birthday, birthplace,
+            religion, status, sex, height, weight, mobile, blood_type, email)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """
         insert_list_query = """
-            INSERT INTO emp_list_id (empl_no, empl_id, taxstat, sss, tin, pagibig, philhealth, account_no)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-        """
-        insert_emp_status_query = """
-            INSERT INTO emp_status (empl_no, empl_id, compcode, dept_code, position, emp_stat, date_hired, resigned, dtresign)
+            INSERT INTO emp_list_id (empl_no, empl_id, taxstat, sss, tin, pagibig, philhealth, bank_code, cola)
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        insert_posnsched_query = """
+            INSERT INTO emp_posnsched (empl_no, empl_id, pos_descr, dept_name, sched_in, sched_out)
+            VALUES (%s, %s, %s, %s, %s, %s)
         """
 
         for row_idx in range(1, sheet.nrows):
             row = sheet.row_values(row_idx)
 
-            empno = row[headers.index('empl_no')] if 'empl_no' in headers else ''
+            empno = str(row[headers.index('empno')]) if 'empno' in headers and isinstance(
+                row[headers.index('empno')], (str, float)) else ''
             if not empno:
-                logging.warning(f"Skipping row {row_idx + 1} due to missing empl_no.")
+                logging.warning(f"Skipping row {row_idx + 1} due to missing empno.")
                 continue
 
-            # Process empl_name into surname and firstname
-            empl_name = row[headers.index('empl_name')] if 'empl_name' in headers else ''
-            surname, firstname = '', ''
-            if ',' in empl_name:
-                surname, firstname = [name.strip() for name in empl_name.split(',', 1)]
-            else:
-                surname = empl_name.strip()
-
-            # Process birthday into YYYY-MM-DD format
-            birthday = row[headers.index('birthday')] if 'birthday' in headers else ''
-            formatted_birthday = format_birthday(birthday)
 
             emer_data = {
-                key: row[headers.index(emergency_list_mapping[key])] if key in emergency_list_mapping else ''
+                key: str(row[headers.index(emergency_list_mapping[key])]) if key in emergency_list_mapping and isinstance(
+                    row[headers.index(emergency_list_mapping[key])], (str, float)) else ''
                 for key in emergency_list_mapping.keys()
             }
             personal_data = {
-                key: row[headers.index(emp_info_column_mapping[key])] if key in emp_info_column_mapping else ''
+                key: str(row[headers.index(emp_info_column_mapping[key])]) if key in emp_info_column_mapping and isinstance(
+                    row[headers.index(emp_info_column_mapping[key])], (str, float)) else ''
                 for key in emp_info_column_mapping.keys()
             }
-            personal_data['surname'] = surname
-            personal_data['firstname'] = firstname
-            personal_data['birthday'] = formatted_birthday
 
             list_data = {
-                key: row[headers.index(list_column_mapping[key])] if key in list_column_mapping else ''
+                key: str(row[headers.index(list_column_mapping[key])]) if key in list_column_mapping and isinstance(
+                    row[headers.index(list_column_mapping[key])], (str, float)) else ''
                 for key in list_column_mapping.keys()
             }
-            empstatus_data = {
-                key: row[headers.index(empstatus_column_mapping[key])] if key in empstatus_column_mapping else ''
-                for key in empstatus_column_mapping.keys()
+
+            sche_name = str(row[headers.index('sche_name')]) if 'sche_name' in headers and isinstance(
+                row[headers.index('sche_name')], (str, float)) else ''
+            sched_in, sched_out = split_schedule(sche_name)
+            posnsched_data = {
+                key: str(row[headers.index(posnsched_column_mapping[key])]) if key in posnsched_column_mapping and isinstance(
+                    row[headers.index(posnsched_column_mapping[key])], (str, float)) else ''
+                for key in posnsched_column_mapping.keys()
             }
+            posnsched_data['sched_in'] = sched_in
+            posnsched_data['sched_out'] = sched_out
+
+            logging.debug(f"Inserting into emergency_list: {emer_data}")
+            logging.debug(f"Inserting into emp_info: {personal_data}")
+            logging.debug(f"Inserting into emp_list_id: {list_data}")
+            logging.debug(f"Inserting into emp_posnsched: {posnsched_data}")
 
             try:
-                cursor.execute(insert_emergency_query, (emer_data['empl_no'], emer_data['empl_id']))
+                cursor.execute(insert_emergency_query, (
+                    emer_data['empl_no'], emer_data['empl_id'], emer_data['emer_name']))
                 cursor.execute(insert_personal_query, (
-                    personal_data['empl_no'], personal_data['empl_id'], personal_data['idnum'],
-                    personal_data['surname'], personal_data['firstname'], personal_data['street'],
-                    personal_data['city'], personal_data['zipcode'], personal_data['birthday'],
-                    personal_data['status'], personal_data['sex'], personal_data['telephone']
+                    personal_data['empl_no'], personal_data['empl_id'], personal_data['surname'],
+                    personal_data['firstname'], personal_data['mi'], personal_data['addr1'],
+                    personal_data['birthday'], personal_data['birthplace'], personal_data['religion'],
+                    personal_data['status'], personal_data['sex'], personal_data['height'],
+                    personal_data['weight'], personal_data['mobile'], personal_data['blood_type'],
+                    personal_data['email']
                 ))
                 cursor.execute(insert_list_query, (
                     list_data['empl_no'], list_data['empl_id'], list_data['taxstat'],
                     list_data['sss'], list_data['tin'], list_data['pagibig'],
-                    list_data['philhealth'], list_data['account_no']
+                    list_data['philhealth'], list_data['bank_code'], list_data['cola']
                 ))
-                cursor.execute(insert_emp_status_query, (
-                    empstatus_data['empl_no'], empstatus_data['empl_id'], empstatus_data['compcode'],
-                    empstatus_data['dept_code'], empstatus_data['position'], empstatus_data['emp_stat'],
-                    empstatus_data['date_hired'], empstatus_data['resigned'], empstatus_data['dtresign']
+                cursor.execute(insert_posnsched_query, (
+                    posnsched_data['empl_no'], posnsched_data['empl_id'], posnsched_data['pos_descr'],
+                    posnsched_data['dept_name'], posnsched_data['sched_in'], posnsched_data['sched_out']
                 ))
 
                 connection.commit()
--- TimeKeeping/paytimeSheet/paytimeSheet.py
+++ TimeKeeping/paytimeSheet/paytimeSheet.py
@@ -11,7 +11,6 @@ def resource_path(relative_path):
         base_path = sys._MEIPASS2
     except Exception:
         base_path = os.path.abspath(".")
-
     return os.path.join(base_path, relative_path)
 
 # Configure the logger
@@ -23,12 +22,11 @@ class PaytimeSheet(QMainWindow):
     def __init__(self, data, fromDate, toDate):
         super(PaytimeSheet, self).__init__()
         self.setFixedSize(1700, 665)
-        ui_file = (resource_path("TimeKeeping\\paytimeSheet\\paytimesheet.ui"))
+        ui_file = resource_path("TimeKeeping\\paytimeSheet\\paytimesheet.ui")
         loadUi(ui_file, self)
 
         self.data = data
 
-        # Make the column headers fixed size
         self.paytimesheetTable.horizontalHeader().setSectionResizeMode(QHeaderView.Fixed)
         self.paytimesheetTable.horizontalHeader().setStretchLastSection(True)
 
@@ -37,19 +35,26 @@ class PaytimeSheet(QMainWindow):
 
         self.populatePaytimeSheetTable(self.data)
 
-
     def populatePaytimeSheetTable(self, data):
         self.paytimesheetTable.setRowCount(len(data))
 
         for i, row in enumerate(data):
+            emp_no_item = QTableWidgetItem(str(row['EmpNo']))
             bio_num_item = QTableWidgetItem(row['BioNum'])
+            emp_name_item = QTableWidgetItem(row['EmpName'])
+            emp_name_item.setToolTip(row['EmpName'])
             present_days_item = QTableWidgetItem(str(row['Present Days']))
             present_holidays_item = QTableWidgetItem(str(row['Present Holidays']))
 
-            # Centers the all the item
-            for item in [bio_num_item, present_days_item, present_holidays_item]:
+            # Centers all the items
+            for item in [emp_no_item, bio_num_item, emp_name_item, present_days_item, present_holidays_item]:
                 item.setTextAlignment(Qt.AlignCenter)
 
+            # Logging the row data being added
+            logging.info(f"Adding row {i}: {row}")
+
+            self.paytimesheetTable.setItem(i, 0, emp_no_item)
             self.paytimesheetTable.setItem(i, 1, bio_num_item)
-            self.paytimesheetTable.setItem(i, 5, present_days_item)
+            self.paytimesheetTable.setItem(i, 2, emp_name_item)
+            self.paytimesheetTable.setItem(i, 4, present_days_item)
             self.paytimesheetTable.setItem(i, 7, present_holidays_item)
