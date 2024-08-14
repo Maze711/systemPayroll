98fde246b3245f55a71638b18f0962013ff1670c
Rods777 <rodelcuyag123@gmail.com>
2024-08-14 16:10:07 +0800

    Optimizing sending bug report

--- MainFrame/bugReport.py
+++ MainFrame/bugReport.py
@@ -1,4 +1,5 @@
 from MainFrame.Resources.lib import *
+from email.utils import formataddr
 from MainFrame.Database_Connection.user_session import UserSession
 from MainFrame.systemFunctions import globalFunction, single_function_logger
 
@@ -33,10 +34,11 @@ class BugReportModal(QDialog):
         msg_box.setIcon(QMessageBox.Information)
         msg_box.setText("Sending the report. Please wait...")
         msg_box.setWindowTitle("Sending Report")
-        msg_box.setStandardButtons(QMessageBox.NoButton) # disabling close button
+        msg_box.setStandardButtons(QMessageBox.NoButton)  # disabling close button
         msg_box.show()
-        QApplication.processEvents() # Ensure the message box is shown
+        QApplication.processEvents()  # Ensure the message box is shown
 
+        sender_name = str(self.user_session["user_name"])
         email_sender = str(self.user_session["user_email"])
         email_password = str(self.user_session["user_email_password"])
 
@@ -54,41 +56,50 @@ class BugReportModal(QDialog):
             with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
                 smtp.login(email_sender, email_password)
 
-                for each_email in developers_email:
-                    em = EmailMessage()
-                    em['From'] = email_sender
-                    em['To'] = each_email
-                    em['Subject'] = subject
-                    em.set_content(body)
+                em = EmailMessage()
+                em['From'] = formataddr((f"{sender_name}", f"{email_sender}"))  # Displays the user_name as sender
+                em['Bcc'] = ', '.join(developers_email)  # Concatenates all dev emails
+                em['Subject'] = subject
+                em.add_alternative(body, subtype='html')
 
-                    if not self.send_email(em, smtp):
-                        logging.error(f"Failed to send email to {each_email}.")
-
-            msg_box.done(QMessageBox.Ok) # closes the wait message
-
-            QMessageBox.information(self, "Report Sent", "Report sent successfully!")
-            self.cancelBtn()
-            logging.info("Bug Report has been sent successfully!")
+                if self.send_email(em, smtp):
+                    msg_box.done(QMessageBox.Ok)  # closes the wait message on success
+                    QMessageBox.information(self, "Report Sent", "Report sent successfully!")
+                    self.cancelBtn()
+                    logging.info("Bug Report has been sent successfully!")
+                else:
+                    msg_box.done(QMessageBox.Ok)
+                    QMessageBox.warning(self, "Sending Bug Report Error",
+                                        "Failed to send the report email. Please try again later.")
+                    logging.error("Failed to send the report email.")
         except smtplib.SMTPException as e:
-            QMessageBox.warning(self, "Sending Bug Report Error", "An SMTP error occurred while sending emails.")
+            msg_box.done(QMessageBox.Ok)
+            QMessageBox.warning(self, "Sending Bug Report Error",
+                                "An SMTP error occurred while sending emails.")
             logging.error(f"SMTP error: {e}")
         except Exception as e:
-            QMessageBox.warning(self, "Sending Bug Report Error", "An unexpected error occurred while sending "
-                                                                      "bug report.")
+            msg_box.done(QMessageBox.Ok)
+            QMessageBox.warning(self, "Sending Bug Report Error",
+                                "An unexpected error occurred while sending the bug report.")
             logging.error(f"Unexpected error: {e}")
 
     def prepare_email_body(self, bug_content):
 
         body = f"""
-        Dear Developers,
-            
-            I hope this email finds you well. I would like to report a bug that I encountered in the system.
-                
-            Bug Description:
-            
-                {bug_content}
+            <html>
+                <body>
+                    <p>Dear Developers,</p>
+
+                    <p>I hope this email finds you well. I would like to report a bug that I encountered in the system.</p>
+
+                    <p><strong>Bug Description:</strong></p>
                     
-        Thank you for your assistance."""
+                    <p>{bug_content}</p>
+
+                    <p>Thank you for your assistance.</p>
+                </body>
+            </html>
+        """
 
         return body
 
@@ -96,8 +107,8 @@ class BugReportModal(QDialog):
     def send_email(self, em, smtp):
         # Sends email to each receiver
         try:
-            smtp.sendmail(em['From'], em['To'], em.as_string())
+            smtp.send_message(em)
         except Exception as e:
-            logging.warning(f"Failed to send email to {em['To']}: {e}")
+            logging.warning(f"Failed to send email: {e}")
             return False
         return True
