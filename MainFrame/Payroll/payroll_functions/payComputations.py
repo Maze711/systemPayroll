672e1a1b71f8c4c90c5924aa9c6799c3d8459d46
s3nkuuu <jhayemcalleja011@gmail.com>
2024-09-01 22:43:21 +0800

    Late and Undertime Computation

--- MainFrame/Payroll/payroll_functions/payComputations.py
+++ MainFrame/Payroll/payroll_functions/payComputations.py
@@ -41,3 +41,24 @@ class PayComputation:
             overtime_value = ordinary_day_ot_float * overtime_rate
             item['OT_Earn'] = round(overtime_value, 2)
             logging.info(f"Calculated overtime for EmpNo {item['EmpNo']}: {item['OT_Earn']}")
+
+    def lateUndertimeComputation(self):
+        late_and_undertime_rate = 59.875  # Define the late and undertime rate
+        for item in self.data:
+            days = item.get('Days')
+            hours = item.get('Hours')
+
+            try:
+                days_float = float(days)
+                hours_float = float(hours)
+            except ValueError:
+                days_float = 0
+                hours_float = 0
+
+            if hours_float > 0:
+                late_undertime_value = (days_float / hours_float) * late_and_undertime_rate
+            else:
+                late_undertime_value = 0
+
+            item['LateUndertime'] = round(late_undertime_value, 2)
+            logging.info(f"Calculated late and undertime for EmpNo {item['EmpNo']}: {item['LateUndertime']}")
